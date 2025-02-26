f0f7ace9932b10b70c1fae20853f8d1e888dde17
Matheus Gusmão <mgusmao.dev@gmail.com>
2025-02-25 23:36:52 -0300

    fix: add the transaction before persisting the object

--- src/main/java/br/com/gusmaomatheus/persistence/dao/H2DatabaseStudentDAO.java
+++ src/main/java/br/com/gusmaomatheus/persistence/dao/H2DatabaseStudentDAO.java
@@ -3,12 +3,19 @@ package br.com.gusmaomatheus.persistence.dao;
 import br.com.gusmaomatheus.model.Student;
 import br.com.gusmaomatheus.persistence.ConnectionFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 
 public final class H2DatabaseStudentDAO implements StudentDAO {
     private final EntityManager entityManager = ConnectionFactory.getEntityManager();
 
     @Override
     public void save(Student student) {
+        final EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
         entityManager.persist(student);
+        transaction.commit();
+
+        entityManager.close();
     }
 }
