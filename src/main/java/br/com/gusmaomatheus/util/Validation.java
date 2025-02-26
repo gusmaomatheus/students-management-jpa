e257ced0f7ba724e2e12bf9afa5c38c052a2ae34
Matheus Gusmão <mgusmao.dev@gmail.com>
2025-02-25 22:59:16 -0300

    feat: create utility class to assist with some validations
    Validations for student email and RA.

---
+++ src/main/java/br/com/gusmaomatheus/util/Validation.java
@@ -0,0 +1,18 @@
+package br.com.gusmaomatheus.util;
+
+import java.util.regex.Pattern;
+
+public final class Validation {
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+    private static final String RA_REGEX = "^SC[0-9X]+$";
+    private static final Pattern RA_PATTERN = Pattern.compile(RA_REGEX);
+
+    public static boolean isValidEmail(String email) {
+        return EMAIL_PATTERN.matcher(email).matches();
+    }
+
+    public static boolean isValidRA(String ra) {
+        return RA_PATTERN.matcher(ra).matches();
+    }
+}
