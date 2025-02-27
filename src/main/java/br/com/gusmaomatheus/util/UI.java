07c9b6b2b27f93fc5ce932ea183ad93f2805ede9
Matheus Gusmão <mgusmao.dev@gmail.com>
2025-02-27 01:21:33 -0300

    refactor: change main menu order

--- src/main/java/br/com/gusmaomatheus/util/UI.java
+++ src/main/java/br/com/gusmaomatheus/util/UI.java
@@ -8,8 +8,8 @@ public final class UI {
     public static void mainMenu() {
         System.out.println("\n** CADASTRO DE ALUNOS **\n");
         System.out.println("1. Cadastrar aluno");
-        System.out.println("2. Editar aluno");
         System.out.println("3. Excluir aluno");
+        System.out.println("2. Editar aluno");
         System.out.println("4. Buscar aluno pelo nome");
         System.out.println("5. Listar alunos (com status de aprovação)");
         System.out.println("6. Encerrar");
