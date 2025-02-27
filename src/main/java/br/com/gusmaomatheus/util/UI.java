c7783590aa1e3ce71ea92afe5886b2960374106e
Matheus Gusmão <mgusmao.dev@gmail.com>
2025-02-27 00:36:00 -0300

    feat: create function that displays Student information from a Student object

--- src/main/java/br/com/gusmaomatheus/util/UI.java
+++ src/main/java/br/com/gusmaomatheus/util/UI.java
@@ -1,5 +1,7 @@
 package br.com.gusmaomatheus.util;
 
+import br.com.gusmaomatheus.model.Student;
+
 public final class UI {
     private UI() {}
 
@@ -11,5 +13,16 @@ public final class UI {
         System.out.println("4. Buscar aluno pelo nome");
         System.out.println("5. Listar alunos (com status de aprovação)");
         System.out.println("6. Encerrar");
+    }
+
+    public static void printStudentInfos(Student student) {
+        System.out.println();
+        System.out.println("Nome: " + student.getName());
+        System.out.println("RA: " + student.getRa());
+        System.out.println("Email: " + student.getEmail());
+        System.out.printf("Notas: %.2f - %.2f - %.2f\n", student.getGrade1().doubleValue(), student.getGrade2().doubleValue(), student.getGrade3().doubleValue());
+        System.out.printf("Média: %.2f\n", student.getAverage());
+        System.out.println("Situação: " + student.getSituation());
+        System.out.println();
     }
 }
